541a32aff2ea39840a3f0ff08d89f04016f81c07
Miguelzzzz <148546151+Miguelzzzz@users.noreply.github.com>
2023-10-24 21:48:34 -0300

    Edição
    css

--- iframe/js/jquery.calendario.js
+++ iframe/js/jquery.calendario.js
@@ -9,7 +9,7 @@
 
   $.Calendario.defaults = {
 
-    weeks: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+    weeks: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
     weekabbrs: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
     months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
     monthabbrs: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
