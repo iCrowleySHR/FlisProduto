edbb7b2f7b22a760e0e431ae471a20f345f1a0b3
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2023-11-06 15:32:38 -0300

    Atualização dos lembretes

--- view/js/lembrete.js
+++ view/js/lembrete.js
@@ -2,6 +2,39 @@ document.addEventListener("DOMContentLoaded", function () {
     const buttonLembrete = document.querySelector(".button-lembrete");
     const lembreteCriado = document.querySelector(".lembrete-criado");
 
+    // Função para carregar lembretes do localStorage
+    function carregarLembretes() {
+        const lembretes = JSON.parse(localStorage.getItem('lembretes')) || [];
+        lembreteCriado.innerHTML = ''; // Limpa a lista de lembretes
+
+        lembretes.forEach((lembrete, index) => {
+            const novoLembreteDiv = document.createElement("div");
+            novoLembreteDiv.classList.add("criados");
+            novoLembreteDiv.innerHTML = `${lembrete.data} <br> ${lembrete.txt}`;
+
+            // Adiciona um botão de exclusão para cada lembrete
+            const btnExcluir = document.createElement("div");
+            btnExcluir.textContent = "Excluir";
+            btnExcluir.classList.add('excluir')
+            btnExcluir.addEventListener("click", () => {
+                lembretes.splice(index, 1); // Remove o lembrete da lista
+                salvarLembretes(lembretes); // Atualiza o localStorage
+                carregarLembretes(); // Atualiza a lista na página
+            });
+
+            novoLembreteDiv.appendChild(btnExcluir);
+
+            lembreteCriado.appendChild(novoLembreteDiv);
+        });
+    }
+
+    // Função para salvar lembretes no localStorage
+    function salvarLembretes(lembretes) {
+        localStorage.setItem('lembretes', JSON.stringify(lembretes));
+    }
+
+    carregarLembretes(); // Carrega lembretes quando a página é carregada
+
     buttonLembrete.addEventListener("click", function () {
         const inserirInfo = document.querySelector('.inserir-info');
         inserirInfo.style.display = 'flex';
@@ -12,12 +45,14 @@ document.addEventListener("DOMContentLoaded", function () {
             const txt = document.querySelector('#input-txt').value;
             const data = document.querySelector('#input-data').value;
 
-            if (txt.trim() && data.trim()) { // Verifica se ambos os campos não estão vazios
-                const novoLembrete = document.createElement("div");
-                novoLembrete.classList.add("criados");
-                novoLembrete.innerHTML = `${data}  <br> ${txt}`;
+            if (txt.trim() && data.trim()) {
+                const lembretes = JSON.parse(localStorage.getItem('lembretes')) || [];
+                const novoLembrete = { data, txt };
+                lembretes.push(novoLembrete);
 
-                lembreteCriado.appendChild(novoLembrete);
+                salvarLembretes(lembretes); // Salva os lembretes no localStorage
+
+                carregarLembretes(); // Atualiza a lista na página
                 inserirInfo.style.display = 'none';
 
                 // Limpa os campos de entrada
