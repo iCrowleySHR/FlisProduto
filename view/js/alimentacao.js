8c908633cc9e93cd69cec3c1455a1aed972e4999
Leo010906 <simonelileandro437@gmail.com>
2023-11-06 23:17:27 -0300

    Inicialização da Página Alimentação
    Adicionei textos e imagens, na imagens tentei criar um carrossel, mas falhei!

---
+++ view/js/alimentacao.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev">Anterior</button>',
+      nextArrow: '<button type="button" class="slick-next">Próxima</button>',
+    });
+  });
+  
