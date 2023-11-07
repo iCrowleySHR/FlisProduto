3046f610163da8ea8c42850ea6ba8905a526888c
Miguelzzzz <148546151+Miguelzzzz@users.noreply.github.com>
2023-11-07 19:48:37 -0300

    edicao
    juntando a parte que o Leandro fez: mudança em alguns arquivos na pasta slick. Criação da pagina de alimentação em HTML, CSS e JS e alteração nos textos da index

--- view/js/alimentacao.js
+++ view/js/alimentacao.js
@@ -1,10 +1,17 @@
-$(document).ready(function() {
-    $('.carousel').slick({
+$(document).ready(function () {
+  $('.carousel').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: true,
       prevArrow: '<button type="button" class="slick-prev">Anterior</button>',
       nextArrow: '<button type="button" class="slick-next">Próxima</button>',
-    });
   });
-  
+
+  $('.texto-carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.carousel',
+  });
+});
