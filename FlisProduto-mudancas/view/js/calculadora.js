2305fd5f3b38d0ffae53e756ca8e716b42ca41ef
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2023-11-07 12:37:00 -0300

    ultima atualization obruigapo

---
+++ FlisProduto-mudancas/view/js/calculadora.js
@@ -0,0 +1,43 @@
+document.querySelectorAll('.container-objetivo button').forEach(button => {
+    button.addEventListener('click', function() {
+        // Remova a classe "selecionado" de todos os botões
+        document.querySelectorAll('.container-objetivo button').forEach(btn => {
+            btn.classList.remove('selecionado');
+        }); 
+
+        // Adicione a classe "selecionado" ao botão clicado
+        this.classList.add('selecionado');
+
+        // Defina o valor da opção selecionada no campo de entrada escondido
+        const valor = this.getAttribute('data-value');
+        document.querySelector('input[name="objetivo"]').value = valor;
+    });
+});
+
+document.getElementById('btn-calccalor').addEventListener('click', calcularCalorias);
+
+function calcularCalorias() {
+    const peso = parseFloat(document.getElementById('peso').value);
+    const idade = parseInt(document.getElementById('idade').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const objetivo = document.querySelector('input[name="objetivo"]').value;
+
+    if (!peso || isNaN(idade) || !altura || !objetivo) {
+        alert('Por favor, preencha todos os campos corretamente.');
+        return;
+    }
+
+    const bmr = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade);
+
+    let tdee;
+    if (objetivo === 'emagrecer') {
+        tdee = bmr * 1.2;
+    } else if (objetivo === 'manter') {
+        tdee = bmr * 1.375;
+    } else if (objetivo === 'engordar') {
+        tdee = bmr * 1.55;
+    }
+
+    const resultadoElement = document.getElementById('resultado');
+    resultadoElement.textContent = `Calorias Diárias: ${tdee.toFixed(2)}`;
+}
---
+++ FlisProduto-mudancas/view/js/script.js
@@ -0,0 +1,63 @@
+let menu = document.querySelector('.menu-icon')
+let navHeader = document.querySelector('.nav-header')
+let visibilidade = false
+let inicioNav = document.querySelector('.inicio')
+
+
+menu.addEventListener('click', ()=>{
+    if(visibilidade == false){
+        visibilidade = true
+        navHeader.style.display='flex'
+    }
+    else{
+        visibilidade = false
+        navHeader.style.display='none'
+    }
+})
+
+function verificarLargura(){
+    let larguraAtual = window.innerWidth
+    let larguraCelular = 1025
+    if(larguraAtual > larguraCelular){
+        navHeader.style.display='flex'
+        inicioNav.setAttribute('class', 'inicio')
+    }else if(larguraAtual < larguraCelular){
+        navHeader.style.display='none'
+        inicioNav.setAttribute('class', '')
+    }
+}
+window.addEventListener('resize', verificarLargura);
+verificarLargura();
+
+/* video */
+let main = document.querySelector('main')
+let videoClick = document.querySelector('.img-video')
+let linkURL = document.querySelector('.boxProduto')
+let iframe = document.querySelector('iframe')
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let main = document.querySelector('main');
+    let iframes = document.querySelectorAll('iframe');
+    let btnSair = document.querySelector('.btn-sair')
+
+    document.addEventListener('click', function(event) {
+        if (event.target.classList.contains('img-video')) {
+            main.style.opacity = '0.2';
+            iframes[0].style.display = 'flex'; // Ajuste conforme necessário se houver vários iframes
+            btnSair.style.display='flex'
+            let iframeLink = event.target.closest('.boxProduto').getAttribute('data-src');
+            iframes[0].setAttribute('src', iframeLink); // Ajuste conforme necessário se houver vários iframes
+
+            function Sair(){
+                btnSair.style.display='none'
+                main.style.opacity = '1';
+                iframes[0].style.display = 'none';
+                iframes[0].setAttribute('src', '');
+            }
+
+            btnSair.addEventListener('click',Sair)
+        }
+    });
+});
+
