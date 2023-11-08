818bf36d3630b6431c3c1e977413515360264fdc
matheussantosrodrigues <matheusdasilva1005@gmail.com>
2023-11-07 21:50:12 -0300

    botando detalhezinho no checkbox

--- view/js/saude.js
+++ view/js/saude.js
@@ -2,7 +2,15 @@ const btnAnalizar = document.querySelector('#btn-analizar');
 
 btnAnalizar.addEventListener('click', () => {
     const elementoIndex = document.querySelector('#text-resultado');
-    const textoIndex = "Sinal, do latim signalis, que significa indício ou manifestação, é um termo usado para referir-se às manifestações clínicas que são reconhecíveis por outra pessoa, em geral, por um profissional da área da saúde, por meio da observação direta do paciente. Nesse caso, são manifestações que o médico ou outra pessoa consegue sentir, visualizar ou ainda escutar.";
+    const textoIndex = "Você pode estar com enxaquecas, frequentemente causam dor latejante que"+
+    " pode ser sentida em um ou ambos os lados da cabeça, incluindo atrás dos olhos. "+
+    "Outros sintomas, como náusea, vômito e sensibilidade à luz e ao som, geralmente acompanham as enxaquecas."+
+    "Se você está enfrentando esses sintomas e "+
+    "eles persistirem ou forem graves, é aconselhável procurar um profissional "+
+    "de saúde para avaliação e diagnóstico adequados. Eles poderão determinar a "+
+    "causa da sua dor de cabeça e recomendar o tratamento apropriado. Lembre-se "+
+    "de que apenas um profissional de saúde pode fornecer um diagnóstico preciso "+
+    "com base em sua história clínica e em um exame físico detalhado.";
 
     if (elementoIndex != null) {
 
@@ -20,7 +28,7 @@ btnAnalizar.addEventListener('click', () => {
 
                 elementoIndex.innerHTML += (proxima);
 
-            }, 60);
+            }, 10);
 
         }
 
